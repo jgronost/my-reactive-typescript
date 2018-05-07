e4748ee305339d5ab9c693c191b2e0a76c343dac
Jaroslaw Gronostajski <jaroslaw,gronostajski@kwsoft.com>
2018-05-07 10:43:18 +0200

    some basic subject examples

---
+++ src/app/behavior-subject.ts
@@ -0,0 +1,21 @@
+import {BehaviorSubject} from "rxjs/index";
+
+function myBehaviourSubject() {
+
+    var subject = new BehaviorSubject(0); // 0 is the initial value
+
+    subject.subscribe({
+        next: (v) => console.log('observerA: ' + v)
+    });
+
+    subject.next(1);
+    subject.next(2);
+
+    subject.subscribe({
+        next: (v) => console.log('observerB: ' + v)
+    });
+
+    subject.next(3);
+}
+
+myBehaviourSubject();
